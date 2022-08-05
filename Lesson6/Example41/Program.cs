de99476d2079702375ca2ef52d132acc8d793383
Grigoriy <g-mayorov1996@mail.ru>
2022-08-05 11:07:51 +0300

    Урок 5 задача 34, 36 и 38

---
+++ Lesson6/Example41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+System.Console.WriteLine("Введите набор чисел");
+string str = System.Console.ReadLine();
+
+string [] array = str.Split(" ");
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (double.Parse(array[i]) > 0) count += 1;
+}
+System.Console.WriteLine($"Количество элементов больше нуля составляет {count}");
---
+++ Lesson6/Example43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 *
+// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 =
+// 9 -> (-0,5; 5,5)
+
+System.Console.WriteLine("Введите значение b1");
+double b1 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите значение k1");
+double k1 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите значение b2");
+double b2 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите значение k2");
+double k2 = int.Parse(System.Console.ReadLine());
+
+System.Console.WriteLine();
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+System.Console.WriteLine($"Точка пересечения двух прямых, заданных уравнением "
+                       + $"y = k * x + b имеет координаты x = {x} и y = {y}");
