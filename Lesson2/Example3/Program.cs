2d116db8cd727b14ed99047a2a22136b2ef01012
Grigoriy <g-mayorov1996@mail.ru>
2022-08-05 10:41:00 +0300

    Урок 2 задача 3 и 4 

---
+++ Lesson2/Example3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введи число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ( num < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+while ( num > 1000)
+{
+    num = num / 10;
+}
+int res = num % 10;
+System.Console.WriteLine(res);
