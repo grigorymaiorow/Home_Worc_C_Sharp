b6be60780e0023435a2fdf7ea03f59afc5229520
Grigoriy <g-mayorov1996@mail.ru>
2022-08-05 10:36:17 +0300

    Урок 2 задача 2

---
+++ Lesson2/Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int a = new Random().Next(100,1000);
+System.Console.WriteLine($"число {a}");
+
+int fir = a / 100;
+int sec = a % 10;
+
+int res = fir * 10 + sec;
+System.Console.WriteLine(res);
